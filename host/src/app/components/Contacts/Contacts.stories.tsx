5dea46db513aa8ef91a66d6f31721880faeb6e8c
Henrique Negri <henriquenegri84@gmail.com>
2025-06-05 14:17:39 -0300

    fix: rename contact mock name file

--- host/src/app/components/Contacts/Contacts.stories.tsx
+++ host/src/app/components/Contacts/Contacts.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { Contacts, ContactsProps } from ".";
-import { contactsData } from "../../_mocks/contract.mock";
+import { contactsData } from "../../_mocks/contact.mock";
 
 const meta: Meta<typeof Contacts> = {
   title: "Portfolio/Contacts",
