db3fb8fd778b9833e0bf14f4934abe1bd1f6c1e0
NegriHenrique15 <NegriHenrique15@users.noreply.github.com>
2025-06-05 19:54:42 +0000

    deploy: 03592b1879f8d5129414e3d4df61d05e8171cb69

--- assets/Button.stories-B5SpzRLg.js
+++
@@ -1,6 +0,0 @@
-import{f as e}from"./index-Do9byjsI.js";import{B as s}from"./Button-BH9Uqb4D.js";import"./jsx-runtime-DiklIkkE.js";import"./index-DRjF_FHU.js";const l={title:"Example/Button",component:s,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{backgroundColor:{control:"color"}},args:{onClick:e()}},r={args:{primary:!0,label:"Button"}};var o,t,a;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {
-    primary: true,
-    label: "Button"
-  }
-}`,...(a=(t=r.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const u=["Primary"];export{r as Primary,u as __namedExportsOrder,l as default};
